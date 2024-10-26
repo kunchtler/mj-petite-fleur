95587c6142c3e438cec8f7df58364c6db5382401
kunchtler <leo.kulinski@gmail.com>
2024-10-26 13:05:07 +0200

    Adjusted camera pos

--- src/Simulator.ts
+++ src/Simulator.ts
@@ -62,8 +62,8 @@ class Simulator {
         const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 50);
         const controls = new OrbitControls(camera, renderer.domElement);
         //camera.position.set(0.0796859236518283, 2.466905446060931, -0.003956106766785765);
-        camera.position.set(1.9, 1.1, 0);
-        controls.target.set(0, 1.0, 0);
+        camera.position.set(2.0, 1.5, 0);
+        controls.target.set(0, 1.4, 0);
         controls.update();
 
         const background_color = window.getComputedStyle(canvas).backgroundColor;
