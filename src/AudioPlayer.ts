88c8320f5f1485b541255f092b8934174c1cb8fb
kunchtler <leo.kulinski@gmail.com>
2024-11-04 22:04:39 +0100

    Created TimeConductor (to handle simulator time)

--- src/AudioPlayer.ts
+++ src/AudioPlayer.ts
@@ -1,4 +1,93 @@
 // Si on fait des play/pause constamment, this.media.currentTime n'aura pas forcément le temps de bien s'update.
+//TODO : At some point, rather than CustomEvents, use Signals/Observer library ?
+
+interface TimeConductorParam {
+    min_time?: number;
+    max_time?: number;
+    start_time?: number;
+    playback_rate?: number;
+    autoplay?: boolean;
+}
+
+class TimeConductor {
+    _last_update_time: number;
+    _last_known_time: number;
+    _playbackRate: number;
+    _paused: boolean;
+    _event_target: EventTarget;
+    _timeupdate_interval?: number;
+
+    constructor({
+        start_time = 0,
+        playback_rate = 1,
+        autoplay = false
+    }: TimeConductorParam) {
+        this._last_update_time = performance.now() / 1000;
+        this._last_known_time = start_time;
+        this._playbackRate = playback_rate;
+        this._event_target = new EventTarget();
+        this._paused = true;
+        if (autoplay) {
+            this.play().catch(() => {
+                throw new Error();
+            });
+        }
+    }
+
+    play(): Promise<void> {
+        this._last_update_time = performance.now() / 1000;
+        this._paused = false;
+        this._event_target.dispatchEvent(new CustomEvent("play"));
+        this._timeupdate_interval = setInterval(() => {
+            this._event_target.dispatchEvent(new CustomEvent("timeupdate"));
+        }, 100);
+        return Promise.resolve();
+    }
+
+    pause(): void {
+        this._last_known_time = this.currentTime;
+        this._paused = true;
+        this._event_target.dispatchEvent(new CustomEvent("pause"));
+        clearInterval(this._timeupdate_interval);
+    }
+
+    set currentTime(time: number) {
+        this._last_update_time = performance.now() / 1000;
+        this._last_known_time = time;
+    }
+
+    get currentTime(): number {
+        if (this.paused) {
+            return this._last_known_time;
+        } else {
+            return (
+                this._last_known_time +
+                (performance.now() / 1000 - this._last_update_time) * this._playbackRate
+            );
+        }
+    }
+
+    set playbackRate(value: number) {
+        //Compute last known time *before* setting playbackrate
+        //as playbackrate is used in currentTime calculation.
+        this._last_known_time = this.currentTime;
+        this._last_update_time = performance.now() / 1000;
+        this._playbackRate = value;
+    }
+
+    get playbackRate(): number {
+        return this._playbackRate;
+    }
+
+    get paused(): boolean {
+        return this._paused;
+    }
+
+    get playing(): boolean {
+        return !this._paused;
+    }
+}
+
 class MediaPlayer {
     media: HTMLMediaElement;
     _last_update_time: number;
@@ -66,4 +155,4 @@ class MediaPlayer {
     }
 }
 
-export { MediaPlayer };
+export { TimeConductor, MediaPlayer };
