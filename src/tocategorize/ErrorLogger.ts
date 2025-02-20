d4ed47ab4b2b79cfd7fc4f45a4e63eeba66b342c
kunchtler <leo.kulinski@gmail.com>
2025-02-20 05:02:51 +0100

    Created Error Logger with chronology

---
+++ src/tocategorize/ErrorLogger.ts
@@ -0,0 +1,54 @@
+import Fraction from "fraction.js";
+import { FracTimeline } from "./mj_parser";
+
+export type Severity = "Log" | "Warn" | "Error" | "CriticalError";
+export type ErrorLog = [Severity, string];
+
+export class TimedErrorLogger {
+    timeline = new FracTimeline<ErrorLog[]>();
+    failedCritically = false;
+
+    constructor() {
+        this.reset();
+    }
+
+    addError(time: Fraction, severity: Severity, message: string): void {
+        if (severity === "CriticalError") {
+            this.failedCritically = true;
+        }
+        const error: ErrorLog = [severity, message];
+        const it = this.timeline.find(time);
+        if (!it.isAccessible()) {
+            this.timeline.setElement(time, [error]);
+        }
+        it.pointer[1].push(error);
+    }
+
+    sortErrors(): [Fraction, ErrorLog[]][] {
+        const sortedErrors: [Fraction, ErrorLog[]][] = [];
+        for (const elem of this.timeline) {
+            sortedErrors.push(elem);
+        }
+        return sortedErrors;
+    }
+
+    logErrors(): void {
+        for (const [time, errors] of this.timeline) {
+            for (const [severity, message] of errors) {
+                const text = `Time ${time.toString()}:\n\t${message}`;
+                if (severity === "Log") {
+                    console.log(text);
+                } else if (severity === "Warn") {
+                    console.warn(text);
+                } else {
+                    console.error(text);
+                }
+            }
+        }
+    }
+
+    reset(): void {
+        this.timeline.clear();
+        this.failedCritically = false;
+    }
+}
