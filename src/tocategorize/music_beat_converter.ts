7938e86e4f64d21743de32e4e7c0ffea0860bfa8
kunchtler <leo.kulinski@gmail.com>
2025-03-10 16:53:33 +0100

    Added ability to havs music tempo info in beat converter

--- src/tocategorize/music_beat_converter.ts
+++ src/tocategorize/music_beat_converter.ts
@@ -1,35 +1,81 @@
 import Fraction from "fraction.js";
 import { Timeline } from "../simulator/Timeline";
 
+//TODO : Messages d'erreurs avec position.
+
+/** @constant
+The epsilon value to use for comparisons ont the timeline.
+Two events apart by less than 0.0001 s are considered to be the same.
+*/
+const EPSILON = 1e-5;
+
+//TODO : Precise in seconds or in milliseconds ?
+// function isApproxEqual(t1: number, t2: number, epsilon = EPSILON): boolean {
+//     return Math.abs(t1 - t2) < epsilon;
+// }
+// function isApproxEqual(f1: Fraction, f2: Fraction, epsilon = EPSILON): boolean {
+//     return f1.sub(f2).abs().lt(epsilon);
+// }
+function timePerMeasure(
+    signature: Fraction,
+    tempoNote: Fraction,
+    tempoBpm: number | Fraction
+): Fraction {
+    return signature.div(tempoNote).mul(tempoBpm);
+}
 //TODO : Make vanilla timeline in utils rather than in simulator folder.
 //TODO : Confusion in what a beat is (if sig 3/4 and tempo 1/4. Is beat : 0, 1, 2 or 0/4, 1/4, 2/4 ??)
 export type MusicTime = [number, Fraction];
 
+//TODO : Change Name.
 export class MusicBeatConverter {
     readonly signatureChanges: Timeline<number, Fraction>;
+    readonly tempoChanges: Timeline<number, { note: Fraction; bpm: number }>;
 
-    constructor(signatureChanges: [number, Fraction][]) {
-        this.signatureChanges = new Timeline(signatureChanges);
+    constructor(
+        signatureChanges: [number, Fraction | string][],
+        tempoChanges: [number, { note: Fraction | string; bpm: number }][]
+    ) {
+        const signatureChangesOnlyFractions = signatureChanges.map(
+            ([measure, beat]) =>
+                [measure, beat instanceof Fraction ? beat : new Fraction(beat)] as [
+                    number,
+                    Fraction
+                ]
+        );
+        this.signatureChanges = new Timeline(signatureChangesOnlyFractions);
         if (this.signatureChanges.empty()) {
             throw Error("Must provide at least one signature.");
+        }
+        const tempoChangesOnlyFraction = tempoChanges.map(
+            ([measure, { note, bpm }]) =>
+                [
+                    measure,
+                    { note: note instanceof Fraction ? note : new Fraction(note), bpm: bpm }
+                ] as [number, { note: Fraction; bpm: number }]
+        );
+        this.tempoChanges = new Timeline(tempoChangesOnlyFraction);
+        if (this.tempoChanges.empty()) {
+            throw Error("Must provide at least one tempo indication.");
         }
     }
 
     // Checks if the beat is not outside the range of the measure.
     validateMusicTime([measure, beat]: MusicTime): boolean {
-        let signature = this.signatureChanges.prev_event(measure, false)[1];
+        let signature = this.signatureChanges.prevEvent(measure, false)[1];
         if (signature === null) {
             signature = this.signatureChanges.begin().pointer[1];
         }
         return beat.lt(signature);
     }
 
-    convertMeasureBeat(time: MusicTime): Fraction {
+    //TODO : Change name.
+    convertMeasureToBeat(musicTime: MusicTime): Fraction {
         // Initial validation for sanity.
-        if (!this.validateMusicTime(time)) {
+        if (!this.validateMusicTime(musicTime)) {
             throw Error("Beat is outside of measure.");
         }
-        const [measure, beat] = time;
+        const [measure, beat] = musicTime;
 
         // In case the measure we search for is before the first documented,
         // we take the initial signature.
@@ -50,7 +96,8 @@ export class MusicBeatConverter {
         return beatAcc.add(currentSignature.mul(measure - currentMeasure)).add(beat);
     }
 
-    convertAbsoluteBeat(beat: Fraction): MusicTime {
+    //TODO : Change name.
+    convertBeatToMeasure(beat: Fraction): MusicTime {
         // Case when the beat is under the first known measure.
         const it = this.signatureChanges.begin();
         let [currentMeasure, currentSignature] = it.pointer;
@@ -78,6 +125,90 @@ export class MusicBeatConverter {
         const beatAnswer = beat.sub(beatAcc);
         return [measureAnswer, beatAnswer];
     }
+
+    convertBeatToRealTime(beat: Fraction): number {
+        const musicTime = this.convertBeatToMeasure(beat);
+
+        const itTempo = this.tempoChanges.begin();
+        const firstTempoMeasure = itTempo.pointer[0];
+        let currentTempo = itTempo.pointer[1];
+        const itSignature = this.signatureChanges.begin();
+        const firstSignatureMeasure = itSignature.pointer[0];
+        let currentSignature = itSignature.pointer[1];
+
+        const minMeasure =
+            firstTempoMeasure < firstSignatureMeasure ? firstTempoMeasure : firstSignatureMeasure;
+
+        let time = timePerMeasure(currentSignature, currentTempo.note, currentTempo.bpm).mul(
+            minMeasure
+        );
+
+        // Case where the beat is before the first documented measure.
+        if (musicTime[0] < minMeasure) {
+            const lastMeasureTime = timePerMeasure(
+                currentSignature,
+                currentTempo.note,
+                currentTempo.bpm
+            );
+            time = time.add(musicTime[1].div(currentSignature).mul(lastMeasureTime));
+            return time.valueOf();
+        }
+
+        // General Case
+        for (let measureIdx = minMeasure; measureIdx < musicTime[0] + 1; measureIdx++) {
+            if (itTempo.isAccessible() && itTempo.pointer[0] === measureIdx) {
+                currentTempo = itTempo.pointer[1];
+                itTempo.next();
+            }
+            if (itSignature.isAccessible() && itSignature.pointer[0] === measureIdx) {
+                currentSignature = itSignature.pointer[1];
+                itSignature.next();
+            }
+            time = time.add(timePerMeasure(currentSignature, currentTempo.note, currentTempo.bpm));
+        }
+        // We've overshot the time by a bit (counting a full measure instead of only the beat).
+        const lastMeasureTime = timePerMeasure(
+            currentSignature,
+            currentTempo.note,
+            currentTempo.bpm
+        );
+        time = time.add(musicTime[1].div(currentSignature).sub(1).mul(lastMeasureTime));
+        return time.valueOf();
+    }
+
+    // convertRealTimeToBeat(time: number, epsilon = EPSILON): Fraction {
+    //     const fracTime = new Fraction(time);
+
+    //     const itTempo = this.tempoChanges.begin();
+    //     const firstTempoMeasure = itTempo.pointer[0];
+    //     let currentTempo = itTempo.pointer[1];
+    //     const itSignature = this.signatureChanges.begin();
+    //     const firstSignatureMeasure = itSignature.pointer[0];
+    //     let currentSignature = itSignature.pointer[1];
+
+    //     const minMeasure =
+    //         firstTempoMeasure < firstSignatureMeasure ? firstTempoMeasure : firstSignatureMeasure;
+    //     const timeAcc = timePerMeasure(currentSignature, currentTempo.note, currentTempo.bpm)
+    //         .mul(minMeasure)
+    //         .valueOf();
+    //     while (timeAcc <= time) {
+    //         if (itTempo.isAccessible() && itTempo.pointer[0] === measureIdx) {
+    //             currentTempo = itTempo.pointer[1];
+    //             itTempo.next();
+    //         }
+    //         if (itSignature.isAccessible() && itSignature.pointer[0] === measureIdx) {
+    //             currentSignature = itSignature.pointer[1];
+    //             itSignature.next();
+    //         }
+    //     }
+
+    //     // Case where the time is before the first known measure.
+
+    //     // General Case
+
+    //     // return new Fraction("0");
+    //     throw new Error("Not Implementsd");
+    // }
 }
 
 //TODO : Use testing library ? Vitest ?
